0ec686bb06485e5360bbeafb6eb8403a9ce81941
baya <meftah.baya@gmail.com>
2017-11-28 10:28:24 +0100

    dépôt initial de mon thème

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/** 
+ * ici je suis sur le fichier par défaut pour tout thème wordpress
+ * il est l'un des 2 obligatoires (l'autre etant style.css)
+ * il est utilisé pour afficher une page si aucun autre fichier n'est disponible 
+ * https : //developer.wordpress.org/themes/basics/template-hierarchy
+ * 
+ * 
+ * 
+ */
+
+get_header();
+get_footer();
+
+
+
+?>
